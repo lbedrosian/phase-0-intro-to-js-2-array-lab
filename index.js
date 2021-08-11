ab31f85ec146ac20798acbca2a5721ad00dc365a
Luke Bedrosian <bedrosian.luke05@gmail.com>
2021-08-11 10:44:59 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph) {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(Bob) {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(Ralph) {
+    cats.pop("Ralph");
+}
+function destructivelyRemoveFirstCat(Bob) {
+    cats.shift("Bob");
+}
+function appendCat(name) {
+    return [...cats, "Broom"];
+}
+function prependCat(name) {
+    return ["Arnold", ...cats];
+}
+function removeLastCat(name) {
+    return [...cats.slice(0, cats.length-1)];
+}
+function removeFirstCat(name) {
+    return cats.slice(1);
+}
